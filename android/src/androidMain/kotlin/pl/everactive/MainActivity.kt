906117ad2c8e303484812f2fe84035d15a586d46
xketris <xketris@gmail.com>
2025-12-31 14:29:34 +0100

    add login ui navigation

--- android/src/androidMain/kotlin/pl/everactive/MainActivity.kt
+++ android/src/androidMain/kotlin/pl/everactive/MainActivity.kt
@@ -1,25 +1,60 @@
 package pl.everactive
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
-            App()
+            EverActiveTheme {
+                AppNavigation()
+            }
         }
     }
 }
 
-@Preview
 @Composable
-fun AppAndroidPreview() {
-    App()
+fun AppNavigation() {
+    var currentScreen by remember { mutableStateOf("login") }
+    val context = LocalContext.current
+
+    when (currentScreen) {
+        "login" -> {
+            LoginScreen(
+                onLoginSuccess = { username ->
+                    Toast.makeText(context, "Logged in as $username", Toast.LENGTH_SHORT).show()
+                },
+                onBackClick = {
+                    currentScreen = "welcome"
+                },
+                onRegisterClick = {
+                    currentScreen = "register"
+                }
+            )
+        }
+        "register" -> {
+            RegisterScreen(
+                onRegisterSuccess = { username, supervisor ->
+                    Toast.makeText(
+                        context,
+                        "Account created for $username\nAlerts sent to: $supervisor",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    currentScreen = "login"
+                },
+                onBackToLoginClick = {
+                    currentScreen = "login"
+                }
+            )
+        }
+    }
 }
