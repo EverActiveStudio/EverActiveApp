ffe7a7228dac918783e57dff56885828730f193a
Szczepan <242479454+szczepan2137@users.noreply.github.com>
2026-01-22 17:55:15 +0100

    feat(android): add role extraction from JWT scopes into EveractiveApiToken

--- android/src/main/kotlin/pl/everactive/clients/EveractiveApiToken.kt
+++ android/src/main/kotlin/pl/everactive/clients/EveractiveApiToken.kt
@@ -1,7 +1,10 @@
 package pl.everactive.clients
 
 import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import pl.everactive.services.DataStoreService
+import java.util.*
 
 class EveractiveApiToken(
     private val dataStoreService: DataStoreService,
@@ -15,7 +18,44 @@ class EveractiveApiToken(
         dataStoreService.remove(TOKEN_KEY)
     }
 
+    // TODO: refactor
+    suspend fun getRole(): Role? {
+        val token = get() ?: return null
+
+        return try {
+            val parts = token.split(".")
+            if (parts.size < 2) return null
+            val decoded = Base64.getUrlDecoder().decode(parts[1])
+            val jsonString = String(decoded, Charsets.UTF_8)
+            val data = json.decodeFromString<TokenData>(jsonString)
+
+            Role.fromScope(data.scope)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    @Serializable
+    data class TokenData(val scope: List<String>)
+
+    enum class Role {
+        User, Manager;
+
+        companion object {
+            fun fromScope(scope: List<String>): Role? = scope.find {
+                it.startsWith("ROLE_")
+            }?.let {
+                when (it.removePrefix("ROLE_")) {
+                    "User" -> User
+                    "Manager" -> Manager
+                    else -> null
+                }
+            }
+        }
+    }
+
     companion object {
         private val TOKEN_KEY = stringPreferencesKey("api_token")
+        private val json = Json { ignoreUnknownKeys = true }
     }
 }
